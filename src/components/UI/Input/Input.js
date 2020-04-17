a9e17ea451fa4877070fc2989ee4668d9c9c2555
Bohn <y2742@softwareag.com>
2020-04-17 18:27:49 +0200

    Visual feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,17 +4,23 @@ import classes from './Input.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
     switch (props.elementType) {
         case ('input'):
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value} 
                 onChange={props.changed}/>;
             break;
         case ('textArea'):
             inputElement = <textarea
-                className={classes.InputElement}
+                className={inputClasses}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed} />;
@@ -22,7 +28,7 @@ const input = (props) => {
         case ('select'):
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={inputClasses}
                     value={props.value}
                     onChange={props.changed}>
                     {props.elementConfig.options.map( option =>(
@@ -34,16 +40,22 @@ const input = (props) => {
             );
             break; default:
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses}
                 {...props.elementConfig}
                 value={props.value} />;
 
+    }
+
+    let validationError = null;
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>
     }
 
     return (
         <div className = {classes.Input}>
             <label className = {classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 };
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -31,7 +32,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false                
+                valid: false ,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -45,7 +47,8 @@ class ContactData extends Component {
                     minlength: 5,
                     maxlength: 5
                 },
-                valid: false                
+                valid: false ,
+                touched: false               
             },
             country: {
                 elementType: 'input',
@@ -57,7 +60,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 } ,
-                valid: false               
+                valid: false ,
+                touched: false              
             },
             email: {
                 elementType: 'input',
@@ -69,7 +73,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 } ,
-                valid: false                   
+                valid: false ,
+                touched: false                  
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -80,6 +85,7 @@ class ContactData extends Component {
                     ]
                 },
                 value : '',
+                touched: false
             }
         },
         loading: false
@@ -133,6 +139,7 @@ class ContactData extends Component {
         }
         updatedFormElement.value = event.target.value;
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         console.log(updatedOrderForm);
         this.setState({orderForm: updatedOrderForm});
@@ -155,6 +162,9 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType} 
                         elementConfig={formElement.config.elementConfig} 
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}
                      />
                 ))}
