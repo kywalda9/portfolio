41cee4349535f837326a464e0ea9c970eaa24521
Bohn <y2742@softwareag.com>
2020-04-27 22:49:17 +0200

    After Fetch Orders (315)

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -21,9 +21,8 @@ export const purchaseBurgerFail = (error) => {
 export const purchaseBurgerStart = () => {
     return {
         type: actionTypes.PURCHASE_BURGER_START
-    }
-    
-}
+    };
+};
 
 //async part
 export const purchaseBurger = (orderData) => {
@@ -45,4 +44,48 @@ export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
     };
+};
+
+
+// sync part order
+export const fetchOrdersSuccess = (orders ) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+//async part
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart())
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch(err => {
+                dispatch(fetchOrdersFail(err));
+            });
+    }
 };
