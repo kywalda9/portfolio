b1346471cca3be5f7aaee182a74aaa670860fe5a
Bohn <y2742@softwareag.com>
2020-04-28 19:23:27 +0200

    auth form inserted

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -126,6 +126,15 @@ class ContactData extends Component {
         if (rules.maxlength) {
             isValid = value.length <= rules.maxlength && isValid
         }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
 
         return isValid;
 
